2d11b68ef59a27ba214768ee80f783b084b1e574
linson_kw <linson.lorance@kilowott.com>
2023-10-28 10:59:22 +0530

    bar chart data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Plot from 'react-plotly.js';
 
-function App() {
+const initialData = [
+  {id: 1, name: 'John', value: 10},
+  {id: 2, name: 'Jane', value: 15},
+  {id: 3, name: 'Bob', value: 20},
+];
+
+const App = () => {
+  const [data, setData] = useState(initialData);
+
+  const handleCheck = (checked, item) => {
+    if (checked) {
+      setData([...data, item]);
+    } else {
+      setData(data.filter(i => i.id !== item.id));
+    }
+  }
+
+  const checkedData = data.map(item => item.value);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          {initialData.map(item => (
+            <tr key={item.id}>
+              <td>
+                <input 
+                  type="checkbox" 
+                  checked={data.find(i => i.id === item.id)}
+                  onChange={e => handleCheck(e.target.checked, item)} 
+                />
+              </td>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.value}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <Plot
+        data={[{
+          x: checkedData,
+          type: 'bar'
+        }]}
+        layout={{width: 720, height: 480, title: 'Bar Chart'}}
+      />
     </div>
   );
 }
